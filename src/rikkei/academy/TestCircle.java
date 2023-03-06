079649d29274cc360fcc7dc6b820f308d4b05b6b
SonJava10Rikkei <sonjapan7@gmail.com>
2023-03-06 15:27:56 +0700

    protected done

--- src/rikkei/academy/TestCircle.java
+++ src/rikkei/academy/TestCircle.java
@@ -1,10 +1,9 @@
 package rikkei.academy;
 
-public class TestCircle extends Circle{
+public class TestCircle {
     public static void main(String[] args) {
 
         Circle circle1 = new Circle();
-
         System.out.println("Radius: " + circle1.getRadius());
         System.out.println("Area: " + circle1.getArea());
     }
